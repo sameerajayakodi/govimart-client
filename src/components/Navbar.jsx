4264ba23749c33ede876149b29190ce151888fd8
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-07-10 11:40:01 +0530

    complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <div>
       <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
-        <NavLink to="/">
+        <NavLink to="/" onClick={() => setOpen(false)}>
           <img className="h-9" src={assets.logo} alt="dummyLogoColored" />
         </NavLink>
 
@@ -32,7 +32,10 @@ const Navbar = () => {
             <img src={assets.search_icon} alt="search" className="w-4 h-4" />
           </div>
 
-          <div className="relative cursor-pointer">
+          <div
+            onClick={() => navigate("/cart")}
+            className="relative cursor-pointer"
+          >
             <img
               src={assets.nav_cart_icon}
               alt="cart"
@@ -103,7 +106,7 @@ const Navbar = () => {
             </NavLink>
             {user && (
               <NavLink
-                to="/products"
+                to="/my-orders"
                 onClick={() => {
                   setOpen(false);
                 }}
