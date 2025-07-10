b66b8798cf91209d36fc18ebb0aa79443cf28e09
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2025-07-10 11:34:59 +0530

    update navbar component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
+import { AppContextProvider } from "./context/AppContext";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <AppContextProvider>
       <App />
-    </BrowserRouter>
-  </StrictMode>
+    </AppContextProvider>
+  </BrowserRouter>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import Navbar from "../components/Navbar";
+
 const Home = () => {
-  return <>Welcome to Homepage</>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default Home;
